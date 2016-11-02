f38d11d6105597184b346dd0955e5800948a2622
Paola Quispe <paola.qmelendez@gmail.com>
2016-11-02 09:49:06 -0500

    agregando pàgina principal, menù dinamico

--- js/main.js
+++ js/main.js
@@ -3,3 +3,33 @@ $(function() {
 		$(".textMove").removeClass("hidden");
 	}, 500);
 });
+
+$(document).ready(function(){
+
+	$("#banner").css({"height":$(window).height() + "px"});
+
+	var flag = false;
+	var scroll;
+
+	$(window).scroll(function(){
+		scroll = $(window).scrollTop();
+
+		if(scroll > 200){
+			if(!flag){
+				$("#logo").css({"margin-top": "-5px", "width": "50px","height":"50px"});
+
+				$("header").css({"background-color": "rgba(0,0,0,0.5)"});
+				flag = true;
+			}
+		}else{
+			if(flag){
+				$("#logo").css({"margin-top": "150px", "width": "250px","height":"250px"});
+
+				$("header").css({"background-color": "transparent"});
+				flag = false;
+			}
+		}
+
+	});
+
+});
